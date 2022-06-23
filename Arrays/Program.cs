e334c2189dbd41d2f04aded5ad4ab7dad99c42b5
Brittanie Campos <bri2212040@maricopa.edu>
2022-06-23 08:49:07 -0700

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿// array = a variable that can store multiple values. fixed size
+
+String[] cars = new string[3];
+
+//String[] cars = { "BMW", "Mustang", "Corvette" };
+
+cars[0] = "Tesla";
+cars[1] = "Mustang";
+cars[3] = "Corvette";
+
+for(int i = 0; i < cars.Length; i++)
+{
+    Console.WriteLine(cars[i]);
+}
+
+Console.ReadKey();
